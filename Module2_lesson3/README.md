24195a9992aac2fce11b1a50f8ebf99641bc6202
Victoria Chukwuno <cheny@pop-os.localdomain>
2022-07-20 20:24:48 +0100

    creating a dictionary in python

---
+++ Module2_lesson3/README.md
@@ -0,0 +1,3 @@
+# Module 2 lesson 3
+## Title: Dictionary
+### Description: writing a python dictionary code containing keys ranging from 5 to 15 (with both numbers included) and the values are the square of the keys. 
--- Module2_lesson3/dictionary.py
+++ Module2_lesson3/dictionary.py
@@ -5,3 +5,11 @@ Date   : 2022-07-13
 Purpose: Dictionary containing keys with values equals to the square of the keys
 """
 
+def square_root():
+    count = 4
+    while (count < 15):
+        count = count + 1
+        print({ count: count ** 2})
+
+
+square_root()
